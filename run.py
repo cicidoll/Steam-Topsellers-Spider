130482777bec79f508b8be8c172135be1a0de42d
cicidoll <54986894+cicidoll@users.noreply.github.com>
2020-07-07 17:25:34 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,40 @@
+from lxml import etree
+from urllib import parse
+import requests
+
+base_url = 'https://store.steampowered.com/search/'
+ua = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'
+
+url_dic = {
+	'filter':'topsellers',
+	'os':'win'
+}
+
+url = "{}?{}".format(base_url,parse.urlencode(url_dic))
+
+with requests.get(url,headers={'User-agent':ua}) as response:
+	content = response.text #HTML内容
+
+	html = etree.HTML(content)#分析HTML，返回DOM根节点
+	topsellers_list_name = html.xpath('//*[@class="col search_name ellipsis"]//span[@class="title"]/text()')
+	topsellers_list_price = html.xpath('//div[@class="col search_price  responsive_secondrow"]/text() | //div[@class="col search_price discounted responsive_secondrow"]/text()')
+
+
+	i = 0#处理topsellers_list_price中，存在的大量空格、换行符和空元素
+	while i < len(topsellers_list_price):
+		topsellers_list_price[i] = topsellers_list_price[i].strip()#处理空格
+		topsellers_list_price[i] = topsellers_list_price[i].strip('\n')#处理换行符
+		topsellers_list_price[i].replace(" ", "")#处理末尾空格
+		for char in topsellers_list_price[i]:
+			if char == '\n' or char == ' ':
+				del char
+		if topsellers_list_price[i] == '':
+			del topsellers_list_price[i]
+		i+=1
+
+
+	print("Steam当前热销榜为：")
+	for i,temp in enumerate(zip(topsellers_list_name,topsellers_list_price)):
+		list_num = i + 1
+		print('-'*30)
+		print("{}.{}".format(list_num,temp))
